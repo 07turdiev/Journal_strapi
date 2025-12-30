e035ea783db6ebca1a4135a30070653e9eee848f
Elbek <elbekturdiyev20020707@gmail.com>
2025-12-30 20:17:15 +0500

    first commit

---
+++ config/middlewares.ts
@@ -0,0 +1,23 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'frame-ancestors': ["'self'", 'http://localhost:5173'], // Vue saytingiz manzili
+        },
+      },
+    },
+  },
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
